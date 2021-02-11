133c8d8476053677543f3b136e388aa83f5e35fe
Rida Ararou <rida@kreatinc.com>
2021-02-11 15:38:05 +0000

    server.js: added auth errorHandlers middlewares expamples

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.set("view engine", "pug")
 // connecting route to database
 app.use(async function(req, res, next) {
   req.con = conn; //await conn.getConnection();
+  req.username = 'Rida'
   next()
 })
 
@@ -24,22 +25,51 @@ app.use(async function(req, res, next) {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
+// app.use(auth)
+// handel erroes
+app.use(logErrors)
+app.use(errorHandler)
 
+// Routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
-// Editor
+// Image Editor
 app.use(express.static(path.join(__dirname, 'client/dist')))
 app.get('/editor', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/dist/index.html'))
 })
 
-
+// User Routes
 app.use('/user', routes);
 
+// middleware fucntions
+//-- auth
+function auth (req, res, next) {
+  console.log('logger', process.env.DB_NAME)
+  if(req.username == 'Rida') {
+    console.log('Authenticated Success')
+    next()
+  }
+  else {
+    console.log('Not Authenticated')
+    res.status(401).send('Not Authenticated')
+  }
+}
+//  errors
+function logErrors (err, req, res, next) {
+  console.error(err.stack)
+  next(err)
+}
+function errorHandler(err, req, res, next) {
+  console.log('errorHander')
+  res.status(500)
+  res.render('error', { error: err })
+}
 
 
+// open server
 app.listen(PORT, () => { console.log('server running at http://localhost:' + PORT )})
 
 
